7f06397259359c0122cb05340a7f0d87fe986f9a
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-06-13 16:24:35 -0400

    change font in Name.js

--- src/elements/Name.js
+++ src/elements/Name.js
@@ -3,9 +3,7 @@ import { Text } from "@react-three/drei";
 const Name = () => {
   return (
     <>
-      <Text font="https://fonts.googleapis.com/css2?family=Crete+Round&family=Fascinate&family=Roboto:wght@900&display=swap">
-        Halsey Quinn
-      </Text>
+      <Text font="helvetiker">Halsey Quinn</Text>
     </>
   );
 };
